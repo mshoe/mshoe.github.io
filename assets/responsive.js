c873a39e7639b3a37a9e6f3e0c2237c5cf6a0fe9
Michael Xu <mxa23@sfu.ca>
2026-06-14 13:11:30 -0700

    Update homepage profile layout

--- assets/responsive.js
+++ assets/responsive.js
@@ -2,6 +2,10 @@ window.onload = function() {
   var sidebar = document.getElementById("sidebar");
   var sidebarButton = document.getElementById("sidebar-button");
 
+  if (!sidebar || !sidebarButton) {
+    return;
+  }
+
   function openSidebar(e) {
     if (sidebar.className.indexOf("slideIn") != -1) {
       sidebar.className = sidebar.className.replace(" slideIn", "");
